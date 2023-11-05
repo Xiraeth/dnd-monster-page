24bc2fa511615c751a98460c1120d155778acc4f
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2023-11-05 22:16:39 +0200

    Implement dice rolls

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ const openMenuIcon = document.querySelector("#openMenu");
 const sidebar = document.querySelector("#sidebar");
 const xButton = document.querySelector(".fa-xmark");
 const stats = document.querySelector(".stats");
+const attackBtn = document.querySelector(".attackRollContainer button");
+const attackRoll = document.querySelector(".attackRoll");
+const damageBtn = document.querySelector(".damageRollContainer button");
+const damageRoll = document.querySelector(".damageRoll");
 
 openMenuIcon.addEventListener("click", function (e) {
   sidebar.style.transform = "translateX(0)";
@@ -33,6 +37,21 @@ stats.addEventListener("input", (e) => {
   getModifier(e.target);
 });
 
+attackBtn.addEventListener("click", function (e) {
+  const roll = generateRandomRoll(20);
+  attackRoll.textContent = roll;
+  changeTextColor(roll, attackRoll);
+});
+
+damageBtn.addEventListener("click", (e) => {
+  const roll1 = generateRandomRoll(WOLF_DMG_DICE);
+  const roll2 = generateRandomRoll(WOLF_DMG_DICE);
+
+  const damage = roll1 + roll2 + 3;
+  damageRoll.textContent = damage;
+  changeTextColor(damage, damageRoll);
+});
+
 function getModifier(abilityScoreElement) {
   let content = abilityScoreElement.textContent;
   let numericContent = content.replace(/\D/g, "");
@@ -44,4 +63,16 @@ function getModifier(abilityScoreElement) {
   let modifierSpan =
     abilityScoreElement.parentElement.querySelector(".modifier");
   modifierSpan.textContent = `(${modifier >= 0 ? "+" : ""}${modifier})`;
+}
+
+function generateRandomRoll(side) {
+  return Math.floor(Math.random() * side) + 1;
+}
+
+function changeTextColor(roll, element) {
+  if (roll == 1) element.style.color = "crimson";
+  if (roll > 1 && roll < 10) element.style.color = "tomato";
+  else if (roll >= 10 && roll < 15) element.style.color = "yellow";
+  else if (roll >= 15 && roll < 20) element.style.color = "rgb(88, 181, 255)";
+  else if (roll == 20) element.style.color = "mediumspringgreen";
 }
